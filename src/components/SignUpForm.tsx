b72490f6bc00d4de5a5fd83fa60e19b4e6b5742e
Alyson Vilela <ctt.alysonvilela@gmail.com>
2025-11-23 15:46:47 +0000

    refactor: clean up SignUpForm component by standardizing formatting and improving readability

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,18 +1,20 @@
-import { useId } from "react";
+'use client'
 
-import { Button } from "@/components/Button";
-import { trackConversion } from "@/components/GoogleScript";
+import { useId } from 'react'
+
+import { Button } from '@/components/Button'
+import { trackConversion } from '@/components/GoogleScript'
 
 export function SignUpForm() {
-	const _id = useId();
+  const _id = useId()
 
-	const handleClick = () => {
-		trackConversion();
-	};
+  const handleClick = () => {
+    trackConversion()
+  }
 
-	return (
-		<form className="relative isolate mt-8 flex items-center pr-1">
-			{/* <label htmlFor={id} className="sr-only">
+  return (
+    <form className="relative isolate mt-8 flex items-center pr-1">
+      {/* <label htmlFor={id} className="sr-only">
         Email address
       </label>
       <input
@@ -24,19 +26,19 @@ export function SignUpForm() {
         placeholder="Email address"
         className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-hidden sm:text-[0.8125rem]/6"
       /> */}
-			<Button
-				type="submit"
-				variant="whatsapp"
-				className="w-full text-white hover:backdrop-opacity-80"
-				href="https://chat.whatsapp.com/HoOyrFuyQAT9rIkNzsrhrD"
-				target="_blank"
-				arrow={false}
-				onClick={handleClick}
-			>
-				Quero receber ofertas exclusivas
-			</Button>
-			{/* <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" /> */}
-			{/* <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" /> */}
-		</form>
-	);
+      <Button
+        type="submit"
+        variant="whatsapp"
+        className="w-full text-white hover:backdrop-opacity-80"
+        href="https://chat.whatsapp.com/HoOyrFuyQAT9rIkNzsrhrD"
+        target="_blank"
+        arrow={false}
+        onClick={handleClick}
+      >
+        Quero receber ofertas exclusivas
+      </Button>
+      {/* <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" /> */}
+      {/* <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" /> */}
+    </form>
+  )
 }
